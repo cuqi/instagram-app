1b03f119002988a41a616c16e31c0efe22cf7985
oppip <filip_pd@hotmail.com>
2019-12-12 14:35:08 +0100

    test za da raboti kaj vlatko

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-console.log("awkudbawkd");
+
 
 
 export class AppComponent {
   title = 'feit-instagram-app';
 
-  public myLog() {
-    
-    console.log("what now?");
-  }
+
 }
