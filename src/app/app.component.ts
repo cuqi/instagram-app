18e848df8f3acb6e8f51976191ff6a944e28f94a
oppip <filip_pd@hotmail.com>
2019-12-26 21:18:25 +0100

    Polished pred dodavanje na api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,18 +27,21 @@ export class AppComponent {
     
     document.getElementById('mainHTML').classList.toggle('dark');      // menjanje na celata pozadina
     var d = document.getElementsByClassName('example-card');        // individualnite postovi
-    var dd = document.getElementsByClassName('commentbutton');
-    var ddd = document.getElementsByClassName('post-comment');
+    var dd = document.getElementsByClassName('commentbutton');      //Comment zborot vo kopceto
+    var ddd = document.getElementsByClassName('post-comment');      //Font na komentarot 
+    var dddd = document.getElementsByClassName('location');         //location delot
     for (let i = 0; i < d.length; i++) {
       d[i].classList.toggle('example-card-dark');
       d[i].classList.toggle('font');
       dd[i].classList.toggle('font');
       ddd[i].classList.toggle('font');
+      dddd[i].classList.toggle('locationfont');
 
     }
     document.getElementById('toolbar').classList.toggle('mat-toolbar-dark');     // toolbar gore
     document.getElementById('homebutton').classList.toggle('darkfont');  // home kopceto vo toolbarot
-    document.getElementById('searchbar').classList.toggle('darkfont');      // search barot vo toolbarot
+    document.getElementById('searchbar').classList.toggle('darkfont');      // search barot vo toolbarot fontot koga kje se napise nesto
+    document.getElementsByClassName("example-form-field")[0].classList.toggle("darkplaceholder");
 
     if (this.flag == true) {
       (<HTMLImageElement>document.getElementById('logo')).src = "assets/icon.ico";
