95d23b878ad51fe31e5f6c4e9c243895a58a8378
Vlatko <krstik1212@gmail.com>
2019-12-26 23:31:40 +0100

    sreden kod, nema novi features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,26 +10,19 @@ import { Posts } from '../db-data';
 export class AppComponent {
 
   total = Posts;
-
-  title = 'feit-instagram-app';
-
-  myPosts = Posts;
-
-  ngInit() {
-
-  }
-
-  onClickSubmit(formData) {
-    alert('You searched for: ' + formData.search + ', but this webpage is basic, sorry!');
-  }
   flag = true;
+  ngInit() { }
+
+  onClickSubmit(formData) { // search in toolbar
+    alert('You searched for: ' + formData.search + ', but this webpage is kinda basic, sorry for the inconvenience!');
+  }
+
   checkbox1() {  // DarkMode
-    
-    document.getElementById('mainHTML').classList.toggle('dark');      // menjanje na celata pozadina
-    var d = document.getElementsByClassName('example-card');        // individualnite postovi
-    var dd = document.getElementsByClassName('commentbutton');      //Comment zborot vo kopceto
-    var ddd = document.getElementsByClassName('post-comment');      //Font na komentarot 
-    var dddd = document.getElementsByClassName('location');         //location delot
+    document.getElementById('mainHTML').classList.toggle('dark');     // menjanje na celata pozadina
+    const d = document.getElementsByClassName('example-card');        // individualnite postovi
+    const dd = document.getElementsByClassName('commentbutton');      // Comment zborot vo kopceto
+    const ddd = document.getElementsByClassName('post-comment');      // Font na komentarot
+    const dddd = document.getElementsByClassName('location');         // location delot
     for (let i = 0; i < d.length; i++) {
       d[i].classList.toggle('example-card-dark');
       d[i].classList.toggle('font');
@@ -38,16 +31,17 @@ export class AppComponent {
       dddd[i].classList.toggle('locationfont');
 
     }
-    document.getElementById('toolbar').classList.toggle('mat-toolbar-dark');     // toolbar gore
-    document.getElementById('homebutton').classList.toggle('darkfont');  // home kopceto vo toolbarot
-    document.getElementById('searchbar').classList.toggle('darkfont');      // search barot vo toolbarot fontot koga kje se napise nesto
-    document.getElementsByClassName("example-form-field")[0].classList.toggle("darkplaceholder");
 
-    if (this.flag == true) {
-      (<HTMLImageElement>document.getElementById('logo')).src = "assets/icon.ico";
+    document.getElementById('toolbar').classList.toggle('mat-toolbar-dark');    // toolbar gore
+    document.getElementById('homebutton').classList.toggle('darkfont');         // home kopceto vo toolbarot
+    document.getElementById('searchbar').classList.toggle('darkfont');          // search barot vo toolbarot fontot koga kje se napise nesto
+    document.getElementsByClassName('example-form-field')[0].classList.toggle('darkplaceholder');
+
+    if (this.flag === true) {
+      (document.getElementById('logo') as HTMLImageElement).src = 'assets/icon.ico';
       this.flag = false;
     } else {
-      (<HTMLImageElement>document.getElementById('logo')).src = "favicon.ico";
+      (document.getElementById('logo') as HTMLImageElement).src = 'favicon.ico';
       this.flag = true;
     }
   }
--- src/app/model/postDetails.ts
+++ src/app/model/postDetails.ts
@@ -14,20 +14,18 @@ export interface PostDetails {
     likes: number;
     isLiked: boolean;
     flaglike: boolean;
-
-    //likedby: string[];
 }
 
-export function gg(k:PostDetails) { // funkcija za "brishenje" na srceto pri like
-    k.flaglike = false;    
+export function gg(k: PostDetails) {       // funkcija za "brishenje" na srceto pri like
+    k.flaglike = false;
 }
 
-export function addComment(k:PostDetails, comm: any) {      //funkcija za dodavanje na komentar
-    var el:PostComments = {username:"", comment:""};
-    el.username = "Filip Panchevski";
+export function addComment(k: PostDetails, comm: any) {     // funkcija za dodavanje na komentar
+    const el: PostComments = {username: '', comment: ''};
+    el.username = 'Filip Panchevski';
     el.comment = comm;
     k.comments.push(el);
-
+}
 
 
 /*
@@ -49,4 +47,3 @@ export function addComment(k:PostDetails, comm: any) {      //funkcija za dodava
 
     k.comments = newc;
 */
-}
