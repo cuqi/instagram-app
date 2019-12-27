0510efaa3b5a47c52e3e6fbf1f6adc28fc5cbfdd
Vlatko <krstik1212@gmail.com>
2019-12-27 18:14:32 +0100

    dodaden scrolling

--- src/app/feitgram-service.service.ts
+++ src/app/feitgram-service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { PostDetails } from './model/postDetails';
+import { PostDetails, PostComments } from './model/postDetails';
 
 @Injectable({
   providedIn: 'root'
